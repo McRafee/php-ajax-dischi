5d3ce304cb2bc4109891936b2cb088bacbe7259d
Raffaele Riviello <raffaele.riviello@gmail.com>
2020-04-20 23:13:32 +0200

    initial commit

---
+++ dist/php/data.php
@@ -0,0 +1,74 @@
+<?php
+$albums = [
+        [
+            "poster" => "../php-ajax-dischi/dist/img/hotelcalifornia-theeagles.jpeg",
+            "title" => "Hotel California",
+            "author" => "The Eagles",
+            "genre" => "Rock",
+            "year" => "1976"
+        ],
+        [
+            "poster" => "../php-ajax-dischi/dist/img/jaggedlittlepill-alanismorissette.jpeg",
+            "title" => "Jagged Little Pill",
+            "author" => "Alanis Morissette",
+            "genre" => "Alternative",
+            "year" => "1995"
+        ],
+        [
+            "poster" => "../php-ajax-dischi/dist/img/ledzeppeliniv-ledzeppelin.jpeg",
+            "title" => "Led Zeppelin IV",
+            "author" => "Led Zeppelin",
+            "genre" => "Rock",
+            "year" => "1971"
+        ],
+        [
+            "poster" => "../php-ajax-dischi/dist/img/londoncalling-theclash.jpeg",
+            "title" => "London Calling",
+            "author" => "The Clash",
+            "genre" => "Punk",
+            "year" => "1980"
+        ],
+        [
+            "poster" => "../php-ajax-dischi/dist/img/nevermind-nirvana.jpeg",
+            "title" => "Nevermind",
+            "author" => "Nirvana",
+            "genre" => "Alternative",
+            "year" => "1991"
+        ],
+        [
+            "poster" => "../php-ajax-dischi/dist/img/okcomputer-radiohead.jpeg",
+            "title" => "OK Computer",
+            "author" => "Radiohead",
+            "genre" => "Alternative",
+            "year" => "1997"
+        ],
+        [
+            "poster" => "../php-ajax-dischi/dist/img/riseandfallofziggystardust-davidbowie.jpeg",
+            "title" => "Rise and Fall of Ziggy Stardust",
+            "author" => "David Bowie",
+            "genre" => "Rock",
+            "year" => "1972"
+        ],
+        [
+            "poster" => "../php-ajax-dischi/dist/img/rumors-fleetwoodmac.jpeg",
+            "title" => "Rumors",
+            "author" => "Fleetwood Mac",
+            "genre" => "Rock",
+            "year" => "1977"
+        ],
+        [
+            "poster" => "../php-ajax-dischi/dist/img/thedarksideofthemoon-pinkfloyd.jpeg",
+            "title" => "The Dark Side of The Moon",
+            "author" => "Pink Floyd",
+            "genre" => "Rock",
+            "year" => "1973"
+        ],
+        [
+            "poster" => "../php-ajax-dischi/dist/img/whitealbum-thebeatles.jpeg",
+            "title" => "The Beatles (White Album)",
+            "author" => "The Beatles",
+            "genre" => "Rock",
+            "year" => "1968"
+        ]
+    ];
+ ?>
---
+++ src/js/main.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+    // *** LOGIC ENGINE *** //
+
+    // *** HANDLEBARS *** //
+    var source = $("#album-template").html();
+    var albumTemplate = Handlebars.compile(source);
+
+
+    $('.genre-selector').change(function () {
+       var genreSelected = $(this).val();
+       // console.log(genreSelected); //debug
+       if (genreSelected == "") {
+           $('.album').show();
+       } else {
+           $('.album').each(function(){
+               if(genreSelected.toLowerCase() == $(this).data('genre').toLowerCase()) {
+                   $(this).show();
+               } else {
+                   $(this).hide();
+               }
+           });
+       }
+    });
+
+    $.ajax({
+        url: "../php-ajax-dischi/dist/php/server.php",
+        method: "GET",
+        success: function (data) {
+            console.log(data);
+            var albums = data;
+            for (var i = 0; i < albums.length; i++) {
+                var album = albums[i];
+                console.log(album);
+                var albumInformation = {
+                    poster: album.poster,
+                    title: album.title,
+                    author: album.author,
+                    year: album.year,
+                    genre: album.genre
+                }
+                var templateWithData = albumTemplate(albumInformation);
+                $('.albums-container').append(templateWithData);
+            }
+        },
+        error: function () {
+            alert('Error');
+        }
+    })
+
+
+});
